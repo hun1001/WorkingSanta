6c71ae96760db1798e986ce1681f3c6d84154876
KBluePurple <dbdnjstjr613@gmail.com>
2022-01-17 14:12:47 +0900

    DOTween Install

--- Assets/Scripts/Prototype/Delivery/DeliveryGameManager.cs
+++ Assets/Scripts/Prototype/Delivery/DeliveryGameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace Delivery_Prototype
 {
@@ -15,30 +16,46 @@ namespace Delivery_Prototype
 
         [SerializeField] int[] _targetHomes;
         [SerializeField] Text _textFloor;
+
         [SerializeField] Text _textFloorListContent;
         [SerializeField] Button _buttonCloseFloorList;
         [SerializeField] CanvasGroup _canvasGroupFloorList;
 
-        private int currentFloor = 0;
+        [SerializeField] RectTransform _rectTransformElevatorDoorLeft;
+        [SerializeField] RectTransform _rectTransformElevatorDoorRight;
+
+        private int currentFloor = 1;
 
         private void Awake()
         {
             _buttonCloseFloorList.onClick.AddListener(() => closeFloorList());
+            _textFloorListContent.text = "";
             foreach (int targetHome in _targetHomes)
             {
                 _textFloorListContent.text += $"{targetHome}호\n";
             }
+            openFloorList();
         }
 
         private void Start()
         {
-            currentFloor = 0;
+            currentFloor = 1;
             updateUI();
         }
 
         private void Update()
         {
-            
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                if (_canvasGroupFloorList.alpha == 0)
+                {
+                    openFloorList();
+                }
+                else
+                {
+                    closeFloorList();
+                }
+            }
         }
 
         private void updateUI()
@@ -58,11 +75,18 @@ namespace Delivery_Prototype
             _canvasGroupFloorList.blocksRaycasts = true;
         }
 
+        private void openElevatorDoor()
+        {
+
+        }
+
         private IEnumerator moveToNextFloor()
         {
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(2f);
             currentFloor++;
             updateUI();
+
+            
         }
     }
 }
