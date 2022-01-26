f46c3774ed757b7dc5173f43d956a9610f55a49e
KBluePurple <dbdnjstjr613@gmail.com>
2022-01-26 18:56:27 +0900

    전생해보니 스탯창이 생겨나있던 건에 대하여

--- Assets/Scripts/CharacterStat.cs
+++ Assets/Scripts/CharacterStat.cs
@@ -1,9 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Item : int
+{
+    None = 0,
+    InfoKillingLicense = 1 << 0,
+    Hamburger = 1 << 1,
+    Fairly = 1 << 2,
+    Rudolf = 1 << 3,
+    Rayder = 1 << 4,
+} 
+
 public class CharacterStat : MonoSingleton<CharacterStat>
 {
-    
-   
+    public Item Items = Item.None;
+    public int RemainingDays = 0;
+    public int Money = 0;
+
+    public void AddItem(Item item)
+    {
+        Items |= item;
+    }
+
+    public void RemoveItem(Item item)
+    {
+        Items &= ~item;
+    }
+
+    public bool HasItem(Item item)
+    {
+        return (Items & item) == item;
+    }
 }
