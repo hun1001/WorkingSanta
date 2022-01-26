456476daae4ad073b222e244cd3392d3d57a0bd7
Corneas <lanilani7661@gmail.com>
2022-01-26 18:42:37 +0900

    난 햄보칼 수 있어(상점 UI배치&버튼연결)

--- Assets/Scripts/Prototype/Main/MainManager.cs
+++ Assets/Scripts/Prototype/Main/MainManager.cs
@@ -15,11 +15,15 @@ namespace Prototype_Main
         private Sprite Delivery;
         [SerializeField]
         private Text wnd;
+        [SerializeField]
+        private GameObject ShopScroll;
         private bool weekwork = false;
         private int hp = 100;
+
         private void Awake()
         {
             ButtonManager.Instance.AddHandler(this);
+            ShopScroll.gameObject.SetActive(false);
         }
 
         private void OnDeliveryStart()
@@ -37,7 +41,14 @@ namespace Prototype_Main
         }
         private void OnShop()
         {
-
+            if (ShopScroll.gameObject.activeSelf == true)
+            {
+                ShopScroll.gameObject.SetActive(false);
+            }
+            else
+            {
+                ShopScroll.gameObject.SetActive(true);
+            }
         }
         private void OnAlba()
         {
@@ -55,6 +66,30 @@ namespace Prototype_Main
                 weekwork = false;
                 wnd.text = "평일알바";
             }
+        }
+        private void OnPI()
+        {
+            Debug.Log("구매");
+        }
+        private void OnPH()
+        {
+            Debug.Log("구매");
+        }
+        private void OnPF()
+        {
+            Debug.Log("구매");
+        }
+        private void OnPR()
+        {
+            Debug.Log("구매");
+        }
+        private void OnPG()
+        {
+            Debug.Log("구매");
+        }
+        private void OnPC()
+        {
+            Debug.Log("구매");
         }
     }
 }
