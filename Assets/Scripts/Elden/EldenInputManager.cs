1ddfe8f4f3cd49e996e279a37eb801783c5a911c
KBluePurple <dbdnjstjr613@gmail.com>
2022-02-15 18:41:38 +0900

    Input System 구현

---
+++ Assets/Scripts/Elden/EldenInputManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EldenInputManager : MonoBehaviour
+{
+    public event Action<float> OnSwapScreen;
+    private bool _isMouseDown = false;
+    private Vector2 _mouseDownPos;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isMouseDown = true;
+            _mouseDownPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isMouseDown = false;
+            if (OnSwapScreen != null)
+            {
+                OnSwapScreen.Invoke(Vector2.Angle(_mouseDownPos, Input.mousePosition));
+            }
+        }
+    }
+}
