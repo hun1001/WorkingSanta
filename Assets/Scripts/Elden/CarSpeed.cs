e9253ba0b92d27542cd84dc537894c215660e4d2
hun1001 <l01022899266@gmail.com>
2022-02-17 06:26:24 +0900

    원석이가 만든거 적용(아마 씬 저장 안하고 올려서 그런거 같기는 한데 걍 내가 적용해둠. 충돌 추가해둠

--- Assets/Scripts/Elden/CarSpeed.cs
+++ Assets/Scripts/Elden/CarSpeed.cs
@@ -11,4 +11,11 @@ public class CarSpeed : MonoBehaviour
     {
         gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - speed/100);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player"){
+            Debug.Log("사고남");
+        }
+    }
 }
