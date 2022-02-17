ba0400a56bf293efdc2b33d6f8c0953f983125ee
hun1001 <l01022899266@gmail.com>
2022-02-17 20:04:52 +0900

    SpawnManager Update

--- Assets/Scripts/Elden/SpawnManager.cs
+++ Assets/Scripts/Elden/SpawnManager.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    enum CarType{
+        Rider,
+        Police,
+        Hole,
+        Bus,
+        Car
+    }
+
     [SerializeField]
     private GameObject[] carPrefabs;
 
-    
+    private Line line;
+
+    private void Awake()
+    {
+        line = EldenGameManager.Instance.Line;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            GameObject car = Instantiate(carPrefabs[(int)CarType.Car], new Vector3(line.List[Random.Range(0, line.List.Length)].transform.position.x, 6, 0), Quaternion.identity);
+            car.transform.SetParent(null);
+        }
+    }
 }
