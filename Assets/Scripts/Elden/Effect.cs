10a12e4f7c91a92de2361c0fdb953b4ae4796988
zmflqhaos <zmflqhaos123@gmail.com>
2022-03-06 23:55:03 +0900

    RiderMoving

--- Assets/Scripts/Elden/Effect.cs
+++ Assets/Scripts/Elden/Effect.cs
@@ -1,52 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Effect : MonoBehaviour
 {
-    private float playerX;
+    private bool once = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag=="Player")
         {
-            if(gameObject.name.Contains("Rider"))
+            if(gameObject.name.Contains("Rider")&&!once)
             {
-                playerX = collision.gameObject.transform.position.x;
-                StartCoroutine(riderMove());
+                transform.parent.DOMoveX(collision.gameObject.transform.position.x, 0.5f);
+                once = true;
             }
         }
-    }
-
-    private IEnumerator riderMove()
-    {
-        int n = 1;
-        float move = 0;
-        if(playerX > 0 && gameObject.transform.parent.position.x < 0)
-        {
-            move = Mathf.Abs(playerX + gameObject.transform.parent.position.x);
-            n *= 1;
-        }
-        else if(playerX < 0 && gameObject.transform.parent.position.x < 0)
-        {
-            move = Mathf.Abs(playerX - gameObject.transform.parent.position.x);
-            n *= -1;
-        }
-        else if(playerX < 0 && gameObject.transform.parent.position.x > 0)
-        {
-            move = Mathf.Abs(playerX + gameObject.transform.parent.position.x);
-            n *= 1;
-        }
-        else if (playerX > 0 && gameObject.transform.parent.position.x > 0)
-        {
-            move = Mathf.Abs(playerX - gameObject.transform.parent.position.x);
-            n *= -1;
-        }
-        move = move * n;
-        while (playerX == gameObject.transform.parent.position.x)
-        { 
-            gameObject.transform.parent.localPosition = new Vector2(gameObject.transform.parent.position.x+move, gameObject.transform.parent.position.y);
-            yield return new WaitForSeconds(0.1f);
-        }
-
     }
 }
--- Assets/Scripts/Elden/SpawnManager.cs
+++ Assets/Scripts/Elden/SpawnManager.cs
@@ -32,8 +32,8 @@ public class SpawnManager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(1.0f, 1.5f));
-            GameObject car = Instantiate(carPrefabs[(int)CarType.Car], new Vector3(line.List[Random.Range(0, line.List.Length)].transform.position.x, 6, 0), Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(1.2f, 1.5f));
+            GameObject car = Instantiate(carPrefabs[(int)CarType.Rider], new Vector3(line.List[Random.Range(0, line.List.Length)].transform.position.x, 6, 0), Quaternion.identity);
             car.transform.SetParent(null);
             car.transform.SetParent(spawnPoint.transform);
             car.transform.localScale = new Vector3(1, 1, 1);
